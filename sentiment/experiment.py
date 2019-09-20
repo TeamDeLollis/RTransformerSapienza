834b7a7858f70eabf6ad6097bcca14434db06fa0
Lorenz <teamdelollis@gmail.com>
2019-09-20 14:54:10 +0200

    sgd 90%

--- sentiment/experiment.py
+++ sentiment/experiment.py
@@ -9,7 +9,7 @@ import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--cuda', action='store_false')
+parser.add_argument('--cuda', action='store_true')
 parser.add_argument('--batch_size', type=int, default=16)
 parser.add_argument('--dropout', type=float, default=0.35)
 parser.add_argument('--emb_dropout', type=float, default=0.25)
@@ -19,7 +19,7 @@ parser.add_argument('--ksize', type=int, default=9)
 parser.add_argument('--n_level', type=int, default=4)
 parser.add_argument('--log-interval', type=int, default=2500, metavar='N')
 parser.add_argument('--lr', type=float, default=0.001)
-parser.add_argument('--optim', type=str, default='Adam')
+parser.add_argument('--optim', type=str, default='SGD')
 parser.add_argument('--rnn_type', type=str, default='GRU') # o gru?
 parser.add_argument('--d_model', type=int, default=256)
 parser.add_argument('--n', type=int, default=1)
@@ -28,7 +28,7 @@ parser.add_argument('--seed', type=int, default=47474)
 parser.add_argument('--permute', action='store_true', default=False)
 parser.add_argument('--corpus', action='store_true',
                     help='force re-make the corpus (default: False)')
-parser.add_argument('--tied', action='store_false')
+parser.add_argument('--tied', action='store_true')
 
 
 args = parser.parse_args()
@@ -48,6 +48,9 @@ train_y = corpus.trainY
 test_data = corpus.testX
 test_y = corpus.testY
 
+test_dim = 2000
+sequence = np.random.randint(0, high=len(test_data), size=test_dim)
+
 n_words = len(corpus.dictionary)
 
 batch_size = args.batch_size
@@ -58,10 +61,11 @@ dropout = args.dropout
 emb_dropout = args.emb_dropout
 tied = args.tied
 
-model = RT(128, n_words, n_classes, h=args.h, rnn_type=args.rnn_type, ksize=args.ksize,
-    n_level=args.n_level, n=args.n, dropout=args.dropout, emb_dropout=args.dropout)
 
-model.to(device)
+model = RT(args.d_model, n_words, n_classes, h=args.h, rnn_type=args.rnn_type, ksize=args.ksize, n_level=args.n_level,
+           n=args.n, dropout=args.dropout, emb_dropout=args.dropout, tied_weights=args.tied, cuda=args.cuda)
+if args.cuda:
+    model.to(device)
 
 model_name = "d_{}_h_{}_t_{}_ksize_{}_level_{}_n_{}_lr_{}_dropout_{}".format(
             args.d_model, args.h, args.rnn_type, args.ksize, 
@@ -102,7 +106,10 @@ def train(ep):
         output = model(data.unsqueeze(0))
         #print(torch.tensor([target]))
         #print(output)
-        loss = F.nll_loss(output, torch.tensor([target - 1]).cuda())
+        if args.cuda:
+            loss = F.nll_loss(output, torch.tensor([target - 1]).cuda())
+        else:
+            loss = F.nll_loss(output, torch.tensor([target - 1]))
         loss.backward()
         if args.clip > 0:
             torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
@@ -121,24 +128,28 @@ def test():
     model.eval()
     test_loss = 0
     correct = 0
-    test_dim = 2000
     with torch.no_grad():
-        sequence = np.random.randint(0, high=len(test_data), size=test_dim) #test on 500 elements from the test set
+         #test on 500 elements from the test set
         for index in sequence:
             data = test_data[index]
             target = test_y[index]
+
             if args.cuda:
                 data = data.cuda()
             output = model(data.unsqueeze(0))
-            loss = F.nll_loss(output, torch.tensor([target - 1]).cuda())
+            if args.cuda:
+                loss = F.nll_loss(output, torch.tensor([target - 1]).cuda())
+            else:
+                loss = F.nll_loss(output, torch.tensor([target - 1]))
+
             pred = output.data.max(1, keepdim=True)[1]
-            if (pred.item() + 1) == target:
+            if int(pred.item() + 1) == int(target):
                 correct += 1
             else:
                 if np.abs(pred.item() + 1 - target) < 1:
                     correct += 0.5
             test_loss += loss.item()
-
+            print(i)
         message = ('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.1f}%)\n'.format(
             test_loss/test_dim, correct, test_dim,
             100 * correct / test_dim))
