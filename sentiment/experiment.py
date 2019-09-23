8a6003777f9d734c94bec6b63352a56a1781971e
TeamDeLollis <54811620+TeamDeLollis@users.noreply.github.com>
2019-09-23 17:44:57 +0200

    Update experiment.py

--- sentiment/experiment.py
+++ sentiment/experiment.py
@@ -21,9 +21,9 @@ parser.add_argument('--log-interval', type=int, default=2500, metavar='N')
 parser.add_argument('--lr', type=float, default=0.001)
 parser.add_argument('--optim', type=str, default='Adam')
 parser.add_argument('--rnn_type', type=str, default='GRU') # o gru?
-parser.add_argument('--d_model', type=int, default=1)
+parser.add_argument('--d_model', type=int, default=64)
 parser.add_argument('--n', type=int, default=1)
-parser.add_argument('--h', type=int, default=1)
+parser.add_argument('--h', type=int, default=8)
 parser.add_argument('--seed', type=int, default=47474)
 parser.add_argument('--permute', action='store_true', default=False)
 parser.add_argument('--corpus', action='store_true',
